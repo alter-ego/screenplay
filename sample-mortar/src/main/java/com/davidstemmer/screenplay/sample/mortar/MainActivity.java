5e99465c3e145f566f8a2aaae83e58f314748822
Ivan Morgillo <imorgillo@gmail.com>
2015-03-22 12:46:52 +0100

    Modify NavigationMenuPresenter to handle MaterialDesign Drawer

--- sample-mortar/src/main/java/com/davidstemmer/screenplay/sample/mortar/MainActivity.java
+++ sample-mortar/src/main/java/com/davidstemmer/screenplay/sample/mortar/MainActivity.java
@@ -3,6 +3,8 @@ package com.davidstemmer.screenplay.sample.mortar;
 import com.davidstemmer.screenplay.SimpleActivityDirector;
 import com.davidstemmer.screenplay.flow.Screenplay;
 import com.davidstemmer.screenplay.sample.mortar.module.ApplicationComponent;
+import com.davidstemmer.screenplay.sample.mortar.presenter.NavigationMenuPresenter;
+import com.davidstemmer.screenplay.scene.Scene;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
@@ -14,6 +16,7 @@ import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,11 +24,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 
+import javax.inject.Inject;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import flow.Flow;
 
 public class MainActivity extends ActionBarActivity {
+
+    private final Handler mDrawerHandler = new Handler();
 
     DrawerLayout mNavigationDrawer;
 
@@ -34,6 +41,15 @@ public class MainActivity extends ActionBarActivity {
 
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
+
+    @Inject
+    Scene mWelcomeScene;
+
+    @Inject
+    Scene mPagedScene;
+
+    @Inject
+    Scene mStackedScene;
 
     private Flow mFlow;
 
@@ -55,6 +71,9 @@ public class MainActivity extends ActionBarActivity {
 
         setSupportActionBar(mToolbar);
         createDrawer();
+
+        NavigationMenuPresenter navigationMenuPresenter = component.menuPresenter();
+        navigationMenuPresenter.setDrawer(mDrawer);
 
         SimpleActivityDirector director = component.director();
         director.bind(this, mMain);
@@ -83,25 +102,11 @@ public class MainActivity extends ActionBarActivity {
                 .withToolbar(mToolbar)
                 .withAccountHeader(headerResult)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName(R.string.nav_item_simple_screen).withIcon(R.drawable.ic_home_grey600_24dp),
+                        new PrimaryDrawerItem().withName(R.string.nav_item_simple_screen).withIcon(R.drawable.ic_home_grey600_24dp).withIdentifier(1),
                         new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName(R.string.nav_item_paged_scenes).withIcon(R.drawable.ic_settings_grey600_24dp),
-                        new SecondaryDrawerItem().withName(R.string.nav_item_modal_scenes).withIcon(R.drawable.ic_launcher)
+                        new SecondaryDrawerItem().withName(R.string.nav_item_paged_scenes).withIcon(R.drawable.ic_settings_grey600_24dp).withIdentifier(2),
+                        new SecondaryDrawerItem().withName(R.string.nav_item_modal_scenes).withIcon(R.drawable.ic_launcher).withIdentifier(3)
                 )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-                        if (drawerItem != null) {
-                            if (drawerItem.getIdentifier() == 1) {
-
-                            } else if (drawerItem.getIdentifier() == 2) {
-
-                            } else if (drawerItem.getIdentifier() == 3) {
-
-                            }
-                        }
-                    }
-                })
                 .build();
     }
 
@@ -119,5 +124,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onDestroy() {
         super.onDestroy();
         ButterKnife.reset(this);
+    }
+
+    private void showNextSceneAfterDelay(final Scene nextScene) {
+        // Clears any previously posted runnables, for double clicks
+        mDrawerHandler.removeCallbacksAndMessages(null);
+        mDrawerHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mFlow.replaceTo(nextScene);
+            }
+        }, 250);
+        // The millisecond delay is arbitrary and was arrived at through trial and error
+        mDrawer.closeDrawer();
     }
 }
--- sample-mortar/src/main/java/com/davidstemmer/screenplay/sample/mortar/presenter/NavigationMenuPresenter.java
+++ sample-mortar/src/main/java/com/davidstemmer/screenplay/sample/mortar/presenter/NavigationMenuPresenter.java
@@ -1,96 +1,43 @@
 package com.davidstemmer.screenplay.sample.mortar.presenter;
 
-import com.davidstemmer.screenplay.sample.mortar.R;
 import com.davidstemmer.screenplay.sample.mortar.scene.PagedScene1;
 import com.davidstemmer.screenplay.sample.mortar.scene.StackedScene;
 import com.davidstemmer.screenplay.sample.mortar.scene.WelcomeScene;
 import com.davidstemmer.screenplay.scene.Scene;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import butterknife.OnClick;
 import flow.Flow;
 
-/**
- * Created by weefbellington on 10/25/14.
- */
 @Singleton
-public class NavigationMenuPresenter extends ViewPresenter<ViewGroup> {
+public class NavigationMenuPresenter extends ViewPresenter<ViewGroup> implements Drawer.OnDrawerItemClickListener {
 
-    private final DrawerPresenter drawer;
+    private final Flow mFlow;
 
-    private final Flow flow;
+    private final WelcomeScene mWelcomeScene;
 
-    private final WelcomeScene welcomeScene;
+    private final PagedScene1 mPagedScene;
 
-    private final PagedScene1 pagedScene;
-
-    private final StackedScene stackedScene;
+    private final StackedScene mStackedScene;
 
     private final Handler mDrawerHandler = new Handler();
 
-    private int selected = R.id.nav_item_simple_scene;
-
+    private Drawer.Result mDrawer;
+    
     @Inject
-    public NavigationMenuPresenter(DrawerPresenter drawerPresenter,
-            Flow flow,
-            WelcomeScene welcomeScene,
-            PagedScene1 pagedScene,
-            StackedScene stackedScene) {
-
-        this.drawer = drawerPresenter;
-        this.flow = flow;
-        this.welcomeScene = welcomeScene;
-        this.pagedScene = pagedScene;
-        this.stackedScene = stackedScene;
-    }
-
-    @OnClick(R.id.nav_item_simple_scene)
-    void welcomeClicked(View selected) {
-        setSelected(selected);
-        showNextSceneAfterDelay(welcomeScene);
-        drawer.close();
-    }
-
-    @OnClick(R.id.nav_item_paged_scenes)
-    void pagedScenesClicked(View selected) {
-        setSelected(selected);
-        showNextSceneAfterDelay(pagedScene);
-        drawer.close();
-    }
-
-    @OnClick(R.id.nav_item_modal_scenes)
-    void modalScenesClicked(View selected) {
-        setSelected(selected);
-        showNextSceneAfterDelay(stackedScene);
-        drawer.close();
-    }
-
-    private void setSelected(View selectedView) {
-        setSelected(selectedView.getId());
-    }
-
-    private void setSelected(int id) {
-        selected = id;
-        for (int i = 0; i < getTarget().getChildCount(); i++) {
-            TextView child = (TextView) getTarget().getChildAt(i);
-            if (id == child.getId()) {
-                child.setSelected(true);
-            } else {
-                child.setSelected(false);
-            }
-        }
-    }
-
-    @Override
-    public void take(ViewGroup view) {
-        setSelected(selected);
+    public NavigationMenuPresenter(Flow flow, WelcomeScene welcomeScene, PagedScene1 pagedScene, StackedScene stackedScene) {
+        mFlow = flow;
+        mWelcomeScene = welcomeScene;
+        mPagedScene = pagedScene;
+        mStackedScene = stackedScene;
     }
 
     private void showNextSceneAfterDelay(final Scene nextScene) {
@@ -99,10 +46,33 @@ public class NavigationMenuPresenter extends ViewPresenter<ViewGroup> {
         mDrawerHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                flow.replaceTo(nextScene);
+                mFlow.replaceTo(nextScene);
             }
         }, 250);
-        // The millisecond delay is arbitrary and was arrived at through trial and error
-        drawer.close();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem drawerItem) {
+        if (drawerItem != null) {
+            if (drawerItem.getIdentifier() == 1) {
+                showNextSceneAfterDelay(mWelcomeScene);
+                mDrawer.closeDrawer();
+            } else if (drawerItem.getIdentifier() == 2) {
+                showNextSceneAfterDelay(mPagedScene);
+                mDrawer.closeDrawer();
+            } else if (drawerItem.getIdentifier() == 3) {
+                showNextSceneAfterDelay(mStackedScene);
+                mDrawer.closeDrawer();
+            }
+        }
+    }
+
+    public Drawer.Result getDrawer() {
+        return mDrawer;
+    }
+
+    public void setDrawer(Drawer.Result drawer) {
+        mDrawer = drawer;
+        mDrawer.setOnDrawerItemClickListener(this);
     }
 }
